30f7c77c9460288890f6b614ee51bb7efb48d67f
leesuin <suin0212@gmail.com>
2022-02-26 18:49:30 +0900

    init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
@@ -9,14 +9,10 @@ const users = require("./routes/users");
 
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", index);
 app.use("/users", users);
