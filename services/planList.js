458e91e216f9e7f66fa68c546a0074ee930db445
leesuin <suin0212@gmail.com>
2022-03-10 17:18:22 +0900

    fix: get the fixed plans

--- services/planList.js
+++ services/planList.js
@@ -9,13 +9,14 @@ exports.getPlans = async (userId) => {
     populate: { path: "friends" },
   });
 
+  console.log("list>>", list);
   list.plans.map((plan) => {
     const afterSixTime = new Date(plan.date);
     const nowTime = new Date();
 
     afterSixTime.setHours(afterSixTime.getHours() + 6);
 
-    if (nowTime < afterSixTime) {
+    if (nowTime < afterSixTime && plan.isFixed) {
       plans[plan._id] = {
         place: plan.place,
         date: plan.date,
